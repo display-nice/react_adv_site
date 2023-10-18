05d4f9f91adc7f59748e475bf23f7761b51752e6
wakadaka59 <osrolex@yandex.ru>
2023-10-18 18:44:07 +0500

    product cards update

--- src/pages/search_page/modules/CardList/CardList.tsx
+++ src/pages/search_page/modules/CardList/CardList.tsx
@@ -1,58 +1,198 @@
 import { useAppSelector } from "@src/hook";
 import { BtnAddToFav } from "./BtnAddToFav";
 import { addThinSpacesToNumber, formatPublishDate } from "./utils";
+import { useState, useCallback } from "react";
 
-const createListItems = (data) => {
-	const listItems = data.map((item) => {
+const Product = ({ item }) => {
+	const [isHintVisible, setIsHintVisible] = useState(false);
+
+	const activate = (e) => {
+		e.target.className = "product__navigation-column product__navigation-column--active";
+	};
+	const deactivate = (e) => {
+		e.target.className = "product__navigation-column";
+	};
+
+	const photosAmount = item.photos.length;
+	let morePhotos = photosAmount > 5 ? photosAmount - 5 : null;
+
+	// const fifthColumnMouseEnter = useCallback((e) => {
+	// 	// activate(e);
+	// 	setIsHintVisible(true);
+	// }, []);
+
+	// const fifthColumnMouseLeave = useCallback((e) => {
+	// 	// deactivate(e);
+	// 	setIsHintVisible(false);
+	// }, []);
+
+	const fifthColumnMouseEnter = useCallback((e) => {
+		// activate(e);
+		setIsHintVisible(true);
+	}, []);
+
+	const fifthColumnMouseLeave = useCallback((e) => {
+		// deactivate(e);
+		setIsHintVisible(false);
+		// console.log('пятая колонка');
+	}, []);
+
+	const test = (e) => {
+		// console.log("хинт");
+		// console.log(e.target.getAttribute("data-index"))
+	}
+	const test2 = (e) => {
+		console.log("колонка");
+	}
+
+	const photoNav = item.photos.map((image, index) => {
+		let mouseEnterHandler;
+		let mouseLeaveHandler;
+		let className;
+		// Подсвечиваем активную плитку. По-умолчанию активной должна быть первая.
+		index === 0
+			? (className = "product__navigation-column product__navigation-column--active")
+			: (className = "product__navigation-column");
+		// Условия в зависимости от количества фото в объявлении в базе данных
+		if (photosAmount >= 2 && photosAmount <= 5) {
+			mouseEnterHandler = activate;
+			mouseLeaveHandler = deactivate;
+		} else if (photosAmount > 5) {
+			// Нам нужно не более пяти плиток (колонок) на изображении
+			// Если мы сейчас на пятой и далее, то ничего не возвращаем
+			if (index >= 1 && index <= 3) {
+				mouseEnterHandler = activate;
+				mouseLeaveHandler = deactivate;
+			}
+			if (index === 4) {
+				return (
+					<>
+						<div
+							key={item.name + "_photo_" + index + "_fifth-column"}
+							onMouseEnter={fifthColumnMouseEnter}
+							onMouseLeave={fifthColumnMouseLeave}
+							className={className += " product__navigation-column--fifth"}
+							data-index={index}
+						>
+							<span></span>
+						</div>
+						{isHintVisible && <div className="product__image-more-photo">
+							+{morePhotos} фото
+						</div>}
+					</>
+				);
+			}
+			if (index > 4) return;
+		}
 		return (
-			<li className="results__item product" key={item.name + "_key"}>
-				<BtnAddToFav />
-				<div className="product__image">
-					<div className="product__image-more-photo hidden">+2 фото</div>
-					<img
-						src="img/item1.jpg"
-						srcSet={item.photos[0]}
-						width="318"
-						height="220"
-						alt="Загородный дом с видом на озеро"
-					/>
-					<div className="product__image-navigation">
-						<span className="product__navigation-item product__navigation-item--active"></span>
-						<span className="product__navigation-item"></span>
-						<span className="product__navigation-item"></span>
-						<span className="product__navigation-item"></span>
-						<span className="product__navigation-item"></span>
-					</div>
-				</div>
-				<div className="product__content">
-					<h3 className="product__title">
-						<a href="/#">{item.name}</a>
-					</h3>
-					<div className="product__price">{addThinSpacesToNumber(item.price)} ₽</div>
-					<div className="product__address">
-						{item.address.city + ", " + item.address.street}
-					</div>
-					<div className="product__date">{formatPublishDate(item["publish-date"])}</div>
-				</div>
-			</li>
+			<div
+				key={item.name + "_photo_" + index}
+				onMouseEnter={mouseEnterHandler}
+				onMouseLeave={mouseLeaveHandler}
+				className={className}
+				data-index={index}
+			>
+				<span></span>
+			</div>
 		);
 	});
-	return listItems;
+
+	return (
+		<li className="results__item product" key={item.name + "_key"}>
+			<BtnAddToFav />
+			<div className="product__image">
+				{/* <div className="product__image-more-photo hidden">+{morePhotos} фото</div> */}
+				
+				<img
+					src="img/item1.jpg"
+					srcSet={item.photos[0]}
+					width="318"
+					height="220"
+					alt="Загородный дом с видом на озеро"
+				/>
+				<div className="product__image-navigation">{photoNav}</div>
+			</div>
+			<div className="product__content">
+				<h3 className="product__title">
+					<a href="/#">{item.name}</a>
+				</h3>
+				<div className="product__price">{addThinSpacesToNumber(item.price)} ₽</div>
+				<div className="product__address">{item.address.city + ", " + item.address.street}</div>
+				<div className="product__date">{formatPublishDate(item["publish-date"])}</div>
+			</div>
+		</li>
+		
+	);
 };
 
 export const CardList = () => {
-	// Инициализация. 
+	// Инициализация.
 	// Если ещё не нажимали кнопку "показать" (то есть если нет фильтрованных данных по продуктам),
 	// то показываем все продукты, пришедшие с сервера, в том порядке, в котором они пришли
 	const productsData = useAppSelector((state) => state.SearchPageReducer.productsData);
 	const filteredProductsData = useAppSelector(
 		(state) => state.SearchPageReducer.filteredProductsData
 	);
-	let listItems;
-	if (!filteredProductsData) listItems = createListItems(productsData);
-	if (filteredProductsData) listItems = createListItems(filteredProductsData);
+	let products;
+	if (!filteredProductsData) products = productsData;
+	if (filteredProductsData) products = filteredProductsData;
 
-	return <ul className="results__list">{listItems}</ul>;
+	return (
+		<ul className="results__list">
+			{products.map((item, index) => {
+				return <Product key={item.name + "_key_photo_" + index} item={item} />;
+			})}
+		</ul>
+	);
 };
 
+// <div
+// 	key={item.name + "_photo_" + index}
+// 	onMouseEnter={activate}
+// 	onMouseLeave={deactivate}
+// 	className="product__navigation-column product__navigation-column--active"
+// >
+// 	<span></span>
+// </div>;
 
+{
+	/* <div
+							onMouseEnter={activate}
+							onMouseLeave={deactivate}
+							className="product__navigation-column product__navigation-column--active"
+						>
+							<span></span>
+						</div>
+
+						<div
+							onMouseEnter={activate}
+							onMouseLeave={deactivate}
+							className="product__navigation-column"
+						>
+							<span></span>
+						</div>
+
+						<div
+							onMouseEnter={activate}
+							onMouseLeave={deactivate}
+							className="product__navigation-column"
+						>
+							<span></span>
+						</div>
+
+						<div
+							onMouseEnter={activate}
+							onMouseLeave={deactivate}
+							className="product__navigation-column"
+						>
+							<span></span>
+						</div>
+
+						<div
+							onMouseEnter={activate}
+							onMouseLeave={deactivate}
+							className="product__navigation-column"
+						>
+							<span></span>
+						</div> */
+}
