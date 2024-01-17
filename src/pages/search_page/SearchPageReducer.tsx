9966688a2012acd46fae267202fd6526de8720d4
wakadaka59 <osrolex@yandex.ru>
2024-01-17 11:07:52 +0500

    XS-S styles 1.3 (hovers)

--- src/pages/search_page/SearchPageReducer.tsx
+++ src/pages/search_page/SearchPageReducer.tsx
@@ -172,7 +172,6 @@ const SearchPageSlice = createSlice({
 			state.displayedProducts = action.payload;
 		},
 		setProductCard(state, action) {
-			console.log('setProductCard', action.payload);
 			state.productCard.isVisible = action.payload.isVisible;
 			state.productCard.data = action.payload.data;
 		},
@@ -204,12 +203,10 @@ const SearchPageSlice = createSlice({
 			state.showFiltersXS = false;
 		},
 		addToFav(state, action) {
-			console.log('сработал addToFav');
 			state.favProducts.push(action.payload);
 			addToCookie("favorites", action.payload["id"]);
 		},
 		removeFromFav(state, action) {
-			console.log('сработал removeFromFav');
 			const i = state.favProducts.findIndex(
 				(favProduct) => favProduct["id"] === action.payload["id"]
 			);
